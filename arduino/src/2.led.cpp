bd0bcaa1caae1178686c663f625d320d55913bb9
freshmea <fresmea@naver.com>
2023-09-02 21:23:13 -0700

    led

---
+++ arduino/src/2.led.cpp
@@ -0,0 +1,9 @@
+void setup(){
+    pinMode(13, OUTPUT);
+    pinMode(12, OUTPUT);
+    pinMode(11, OUTPUT);
+}
+
+void loop(){
+    digitalWrite(13, 255);
+}
--- arduino/test/test.ino
+++ arduino/test/test.ino
@@ -1,15 +1,31 @@
-void setup() {
-  Serial.begin(115200);
-  Serial.println("Serial Test !!!!"); 
-  Serial.print("ECHO >> ");
+int GREEN = 11;
+int RED = 10;
+int BLUE = 9;
+
+void setup(){
+    pinMode(GREEN, OUTPUT);
+    pinMode(RED, OUTPUT);
+    pinMode(BLUE, OUTPUT);
 }
 
-void loop() {
+void loop(){
+  for(int i=0;i<256;i++){
+    digitalWrite(GREEN, 0);
+    digitalWrite(RED, i);
+    digitalWrite(BLUE, 0);
+    delay(5);
+    
+  }
+  for(int i=0;i<256;i++){
+    analogWrite(GREEN, i);
+    analogWrite(RED, 0);
+    analogWrite(BLUE, 0);
+    delay(5);
+  }
+  for(int i=0;i<256;i++){
+    analogWrite(GREEN, 0);
+    analogWrite(RED, 0);
+    analogWrite(BLUE, i);
+    delay(5);
+  }
 }
-
-void serialEvent(void) {
-  char data;
-  data = Serial.read();
-  Serial.print(data);
-}
-
