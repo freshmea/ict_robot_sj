7488907337e8fe0b62820bcbe528878b390e0819
freshmea <fresmea@naver.com>
2023-09-02 21:29:24 -0700

    cds

--- arduino/src/2.led.cpp
+++ arduino/src/2.led.cpp
@@ -1,9 +1,31 @@
+int GREEN = 11;
+int RED = 10;
+int BLUE = 9;
+
 void setup(){
-    pinMode(13, OUTPUT);
-    pinMode(12, OUTPUT);
-    pinMode(11, OUTPUT);
+    pinMode(GREEN, OUTPUT);
+    pinMode(RED, OUTPUT);
+    pinMode(BLUE, OUTPUT);
 }
 
 void loop(){
-    digitalWrite(13, 255);
+  for(int i=0;i<256;i++){
+    digitalWrite(GREEN, 0);
+    digitalWrite(RED, i);
+    digitalWrite(BLUE, 0);
+    delay(5);
+    
+  }
+  for(int i=0;i<256;i++){
+    analogWrite(GREEN, i);
+    analogWrite(RED, 0);
+    analogWrite(BLUE, 0);
+    delay(5);
+  }
+  for(int i=0;i<256;i++){
+    analogWrite(GREEN, 0);
+    analogWrite(RED, 0);
+    analogWrite(BLUE, i);
+    delay(5);
+  }
 }
